15358699e8f9861aeb8609e3afb05e9893ae9019
JoaoPedroaac <joao.cintra7@hotmail.com>
2025-06-06 20:35:47 -0300

    Finalizando trabalho Fausto Prints

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,24 +1,15 @@
 import dotenv from 'dotenv'
-dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
+dotenv.config()
 
-import express, { json, urlencoded } from 'express'
+
 import cookieParser from 'cookie-parser'
+import express, { json, urlencoded } from 'express'
 import logger from 'morgan'
-
 const app = express()
-
-// Desabilita o cabeçalho X-Powered-By para dificultar a identificação
-// da tecnologia na qual o back-end foi desenvolvido
 app.disable('x-powered-by')
-
 import cors from 'cors'
-
-// Configurando o CORS para que o back-end aceite
-// requisições vindas das origens indicadas pela
-// variável de ambiente ALLOWED_ORIGINS
 app.use(cors({
   origin: process.env.ALLOWED_ORIGINS.split(','),
-  // Envia de fato quaisquer cookies gerados para o front-end
   credentials: true
 }))
 
@@ -26,31 +17,21 @@ app.use(logger('dev'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
-
-// Rate limiter: limita a quantidade de requisições que cada usuário/IP
-// pode efetuar dentro de um determinado intervalo de tempo
 import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000,    // Intervalo: 1 minuto
-  limit: 20               // Máximo de 20 requisições
+  windowMs: 60 * 1000,    
+  limit: 20            
 })
 
 app.use(limiter)
-
-/*********** ROTAS DA API **************/
-
-// Middleware de verificação do token de autorização
-import auth from './middleware/auth.js'
-app.use(auth)
-
-import carsRouter from './routes/cars.js'
-app.use('/cars', carsRouter)
-
-import customersRouter from './routes/customers.js'
-app.use('/customers', customersRouter)
-
-import usersRouter from './routes/users.js'
-app.use('/users', usersRouter)
+import authMiddleware from './middleware/auth.js'
+ app.use(authMiddleware)
+  import carsRouter from './routes/cars.js'
+   app.use('/cars', carsRouter)
+    import customersRouter from './routes/customers.js'
+     app.use('/customers', customersRouter)
+      import usersRouter from './routes/users.js'
+       app.use('/users', usersRouter)
 
 export default app
